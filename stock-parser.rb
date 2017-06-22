5269c12a549b7901fa89c608ac222feee18aafc7
Felix Tjandrawibawa <felix.tjandrawibawa@fairfaxmedia.com.au>
2017-06-22 17:43:16 +1000

    Daily commit

--- stock-parser.rb
+++ stock-parser.rb
@@ -1,4 +1,14 @@
 require 'csv'
+require 'sequel'
 
 # we could read and store in the database
-rows = CSV.read("data/20170619.csv")
+# 1. store data in the database
+# 2. do some crunching daily - send email
+# format of the column:
+# code, date, open, high, low, close, volume
+rows = CSV.read('data/20170619.csv')
+
+db = Sequel.connect('mysql2://root@localhost')
+db.execute('CREATE DATABASE IF NOT EXISTS stocks')
+db = Sequel.connect('mysql2://root@localhost/stocks')
+
